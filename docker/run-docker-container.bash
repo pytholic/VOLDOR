970918cc879d4b2d230b841222b7a2dfe967c8c2
pytholic <rajahaseeb147@gmail.com>
2022-12-29 11:16:27 +0900

    added docker support, updated requirements

---
+++ docker/run-docker-container.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# start sharing xhost
+xhost +local:root
+
+# run docker
+docker run \
+  --name voldor \
+  --ipc=host \
+  --gpus all \
+  --privileged \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v $HOME/.Xauthority:$docker/.Xauthority \
+  -e DISPLAY=$DISPLAY \
+  -e XAUTHORITY=$home_folder/.Xauthority \
+  -e QT_X11_NO_MITSHM=1 \
+  -it pytholic/voldor
